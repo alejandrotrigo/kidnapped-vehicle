e44f752b3dc2bb004ff65659711634403ec1886b
Alejandro <alejandro.trigo.roson@gmail.com>
2017-08-05 21:52:27 +0200

    update weights step done

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -118,13 +118,63 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	
 	// p(xt|z1:t) = p(zt|xt) * p(xt|z1:t-1)
 	
-	//Updating weight of a particle:
-	// w = Product (m,i=1) (exp(-1/2 * (xi - ui)^TE-1(xi-ui) / sqrt(|2piE|))
-	// xi = ith landmark
-	// ui = predicted measurement
-	// m = total number of measurements 
-	// E = covariance of the measurement; symmetric square matrix contains the variance/uncertanty of each variable in the sensor measurement; inverse diagonal = 0
-	
+	double distance;
+	for (int i = 0; i < particles.size(); i++){
+		
+		vector<LandmarkObs> predictions;
+		
+		for (int j = 0; j < map_landmarks.landmark_list.size(); j++){
+			double lm_x = map_landmarks.landmark_list[j].x;
+			double lm_y = map_landmarks.landmark_list[j].y;
+			double lm_id = map_landmarks.landmark_list[j].id; 
+			distance = dist(particles[i].x, particles[i].y, lm_x, lm_y);
+			
+			if (distance < sensor_range){
+				predictions.push_back(LandmarkObs{lm_id, lm_x, lm_y});
+			}
+		}
+		
+		// Convert observation coordinates from vehicle to mapp
+		vector<LandmarkObs> observations_t;
+		double cos_t = cos(particles[i].theta); 
+		double sin_t = sin(particles[i].theta); 
+		for (int j = 0; j < observations.size(); j++){
+			double o_x = observations[j].x;
+			double o_y = observations[j].y;
+			double tr_x = cos_t * o_x - sin_t * o_y + particles[i].x;
+			double tr_y = sin_t * o_x + cos_t * o_y + particles[i].y;
+			observations_t.push_back(LandmarkObs{tr_x, tr_y});
+		}
+		
+		dataAssociation(predictions, observations_t);
+		
+		//Updating weight of a particle:
+		// w = Product (m,i=1) (exp(-1/2 * (xi - ui)^TE-1(xi-ui) / sqrt(|2piE|))
+		// xi = ith landmark
+		// ui = predicted measurement
+		// m = total number of measurements 
+		// E = covariance of the measurement; symmetric square matrix contains the variance/uncertanty of each variable in the sensor measurement; inverse diagonal = 0
+		for (int j = 0; j < observations_t.size(); j++){
+			
+			double pr_x, pr_y;
+			
+			for (int k=0; k < predictions.size(); k++){
+				if(predictions[k].id == observations_t[j].id){
+					pr_x = predictions[k].x;
+					pr_y = predictions[k].y;
+				}
+			}
+			
+			//Weight update in 5 steps
+			
+			double o_1 = (1/(2*M_PI*std_landmakr[0]*std_landmark[1]))
+			double o_2 = -(pow(pr_x - observations_t[j].x, 2)/(2*pow(std_landmark[0], 2)))
+			double o_3 = pow(pr_y - observations_t[j].y, 2) / (2*pow(std_landmark[1], 2))
+			double o_4 = o1 * exp(o_2 + o_3);
+			
+			particles[i].weight *= o4;
+		}
+	}
 	
 }
 
